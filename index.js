cec804b8d619cbc2a82c8ce5cfc24cec81a073e3
Asipovich <asipovich.iv1999@gmail.com>
2021-09-14 17:03:30 +0200

    instalation

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("start")
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+
+//AFTER INSTALATION MONGODB
+//const mongoose = require('mongoose');
+//add in package.json "mongoose": "*"
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res)=>{res.send('Strona głowna!!')});
+
+app.listen(port, ()=>{
+    console.log("Server has been launch on port: " + port)
+});
